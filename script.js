f4f9554472ee60c70859c2ae3954aa8268e8edc1
arshad012 <forarshad99@gmail.com>
2024-11-01 13:34:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,4 +2,33 @@
 const allItems = document.querySelectorAll('.item');
 allItems.forEach((item) => {
 	item.setAttribute('draggable', true);
+	item.addEventListener('dragstart', () => {
+		drag(event);
+	})
+	item.addEventListener('dragover', () => {
+		allowDrop(event);
+	})
+	item.addEventListener('drop', () => {
+		drop(event);
+	})
 })
+
+let draggedDiv = null;
+function drag(event) {
+	draggedDiv = event.target;
+}
+
+function allowDrop(event) {
+	event.preventDefault();
+}
+
+
+function drop(event) {
+	let targetDiv = event.currentTarget;
+
+	let tempDiv = targetDiv;
+	targetDiv = draggedDiv;
+	draggedDiv = tempDiv;
+}
+
+
